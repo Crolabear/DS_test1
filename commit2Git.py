f9403462080fe7d44559cc349a76f8ab93c5d07c
crolabear <crolabear@gmail.com>
2018-09-04 01:17:08 -0700

    test3

--- commit2Git.py
+++ commit2Git.py
@@ -1,5 +1,11 @@
 import sys
+import os
 
 comment = sys.argv[1]
-git add *
-git commit -m ''
+st1= 'git add *'
+st2 = 'git commit -m %s' %comment
+st3 = 'git push -u origin master'
+
+os.system(st1)
+os.system(st2)
+os.system(st3)
